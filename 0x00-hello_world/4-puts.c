7c40d1c2a4f03ef8aa54b8cc066677550550ff9a
benneth <ezulikebenneth@gmail.com>
2022-09-08 15:33:06 -0700

    Task 4 coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main function
 * this program prints "programming is like building a multilingual puzzle
-* return: 0
+* Return: 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingua puzzle");
 	return (0);
 }
