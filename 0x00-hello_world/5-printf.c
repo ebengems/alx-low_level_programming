2a16125079f27b44dd1c48e3d65be5bc588441cd
benneth <ezulikebenneth@gmail.com>
2022-09-08 16:48:38 -0700

    task 5 redo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,11 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 /**
- *
- * main - Prints "with proper grammar, but the outcome is
- *
- * a piece of art," followed by a new line.
- *
- * 
- *
- * * Return: Always 0.
- *
- * */
-
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
 	return (0);
-
 }
