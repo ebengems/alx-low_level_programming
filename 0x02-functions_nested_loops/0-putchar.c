8ff3755317e370cbf772c92289f85b6e97be38aa
benneth <ezulikebenneth@gmail.com>
2022-09-13 08:35:07 -0700

    Task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,6 +10,7 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
@@ -18,5 +19,3 @@ int main(void)
 
 	return (0);
 }
-
-
